eb7c248ab189c0c13e4055bd8eb09c0d2165455e
Jerome Andrei O. Hontiveros <129578499+hontiverome@users.noreply.github.com>
2023-04-15 18:44:43 +0800

    finished pseudocode

--- vignere-cypher.py
+++ vignere-cypher.py
@@ -1,1 +1,14 @@
 # ACTIVITY2 NO3 PSEUDOCODE WITH GIT COMMITS
+# Convert current positional character to a corresponding letter value (0-25)
+# Modulo 26 to have a range value of 0-25 only
+# Convert the value back to a letter
+# Add/push the cipher letter to the final cipher text
+# Add current position to the keyword, (will serve as the end when the code reaches the last letter)
+# Print the output
+
+# PRODUCE THE CIPHERTEXT OF THE FOLLOWING:
+# Message: THISISTHELASTTASKHOORDAY
+# Key: KNIGHTS
+
+# HONTIVEROS, JEROME ANDREI O.   
+# BSCPE 1-5
