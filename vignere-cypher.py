f4d612c16e38cf481b68793e58b31e5d47474e24
Jerome Andrei O. Hontiveros <129578499+hontiverome@users.noreply.github.com>
2023-04-15 19:03:04 +0800

    positional convesions

--- vignere-cypher.py
+++ vignere-cypher.py
@@ -3,8 +3,11 @@
 Message=""
 KeyWord=""
 CipherText=""
-# Convert current positional character to a corresponding letter value (0-25)
+# Makes the conversion on each position
 KeyValues=[ord(j)-65 for j in KeyWord]
+LetterPosition=0
+# Convert current positional character to a corresponding letter value (0-25)
+LetterValue=ord(j)-65
 # Modulo 26 to have a range value of 0-25 only
 # Convert the value back to a letter
 # Add/push the cipher letter to the final cipher text
